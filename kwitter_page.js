9d8411205d2c659e0980a870cbae46457bc6cc61
sreehan27 <168743610+sreehan27@users.noreply.github.com>
2024-05-04 14:05:32 +0000

    193504052024

--- kwitter_page.js
+++ kwitter_page.js
@@ -39,7 +39,7 @@ function getData() {
                         name = message_data['name'];
                         message = message_data['message'];
                         like = message_data['like'];
-                        name_with_tag = "<h4> " + name + "<img class='user_tick' src='tick.png'></h4>";
+                        name_with_tag = "<h4> " + name + "</h4>";
                         message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
                         like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
                         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
